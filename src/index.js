a084030cf54b3188656e256e1eca847671f5a932
jensiriphone <jennetsiriphone@gmail.com>
2023-11-29 19:15:06 +1300

    added change in console message

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3000;
 const env = process.env.NODE_ENV;
 
 app.get('/', (req, res) => {
-  res.send(`Server ${env} is up and running`);
+  res.send(`Server ${env} is up and running with new change`);
 });
 app.listen(PORT, () => {
   console.log(`Listening to ${PORT}`);
